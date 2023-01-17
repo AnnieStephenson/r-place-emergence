7152b9064ad023f42c1d632cd170d8ad8d7aa126
GuillaumeFa <g.falmagne@princeton.edu>
2023-01-17 16:45:26 -0500

    Some structural changes to CanvasPart. All compositions and the stability function run well.

---
+++ PrepareData/correct_atlas.py
@@ -0,0 +1,17 @@
+import shutil
+
+# Read in the file
+with open('../data/atlas.json', 'r') as file :
+  filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace('\"2b2tplace\"]}, \"path\": {\"1-165, T:0-1\": [[913, 421], [909, 482], [959, 480], [958, 421]]',
+                            '\"2b2tplace\"]}, \"path\": {\"1-165, T:0-1\": [[912, 421], [912, 481], [960, 481], [960, 421]]')
+
+filedata = filedata.replace('{\"id\": \"tykicd\", \"name\": \"Snoo\", \"description\": \"Outside the r/place canvas, There were two Snoo (Reddit\'s mascot) and a dog looking off the edge of the map while on a sled. While not part of the canvas itself, They can be seen in the r/place timelapse.\", \"links\": {\"website\": [\"https://www.reddit.com/r/place/?cx=3&cy=0&px=6&ts=1649112460185\"], \"subreddit\": [\"place\"]}, \"path\": {\"1-164\": [[-2, -3], [4, -3], [4, 0], [-2, 0]]}, \"center\": {\"1-164\": [0, 0]}},', '')
+
+filedata = filedata.replace('\"T\"', '\"T:0-1\"')
+
+# Write the file out again
+with open('../data/atlas.json', 'w') as file:
+  file.write(filedata)
--- rplacem/draw_composition_vs_time.py
+++ rplacem/draw_composition_vs_time.py
@@ -11,31 +11,26 @@ import matplotlib.mlab as mlab
 
 '''
 x1 = 0
-x2 = 2000
+x2 = 100
 y1 = 0
-y2 = 2000
+y2 = 100
 part_name = 'rectangle_'+str(x1)+'.'+str(y1)+'-'+str(x2)+'.'+str(y2)  # 'txd9gh'
 
-border_path = np.array([[x1, y1], [x1, y2], [x2, y2], [x2, y1]])
-canvas_area = cp.CanvasArea(border_path, None,
+border_path = np.array([[[x1, y1], [x1, y2], [x2, y2], [x2, y1]]])
+canvas_part = cp.CanvasPart(id='000021', 
+                            #border_path=border_path, 
+                            pixel_changes_all=None,
                             data_path=os.path.join(os.getcwd(), 'data'), data_file='PixelChangesCondensedData_sorted.npz')
+print(canvas_part)
+print(canvas_part.border_path)
+print(canvas_part.border_path_times)
 '''
 
-file_path = os.path.join(os.getcwd(),'data', 'canvas_compositions_all.pickle') 
+data_path = os.path.join(os.getcwd(),'data')
+file_path = os.path.join(data_path, 'canvas_compositions_all.pickle') 
 
 with open(file_path, 'rb') as f:
     canvas_parts = pickle.load(f)
-
-'''
-canvas_comp = canvas_parts[8400] #cp.CanvasComposition('000006', None,data_path=os.path.join(os.getcwd(),'data'),data_file='PixelChangesCondensedData_sorted.npz')
-part_name = canvas_comp.id
-
-'''
-#print('save_part_over_time')
-#from pstats import SortKey
-
-#pr = cProfile.Profile()
-#pr.enable()
 
 '''
 try:
@@ -49,7 +44,11 @@ except OSError:
     print('')
 '''
 
-#file_size_bmp, file_size_png, t_inds_list = cp.save_part_over_time(canvas_comp, 300, total_time=var.TIME_TOTAL, part_name=part_name, delete_bmp=True, delete_png=False, show_plot=False)
+#time_intervals = np.arange(0, var.TIME_TOTAL, 300)
+#file_size_bmp, file_size_png, t_inds_list = cp.save_part_over_time(canvas_part, time_intervals, delete_bmp=True, delete_png=False, show_plot=False)
+#cp.save_movie(os.path.join(os.getcwd(),'figs','history_'+canvas_part.id,'VsTime'), 15)
+
+
 
 time_bins = 80
 time_interval = var.TIME_WHITEONLY / time_bins  # seconds
@@ -57,33 +56,49 @@ time_ranges = np.arange(0, var.TIME_WHITEONLY+time_interval-1e-4, time_interval)
 
 mean_stability = []
 stability_vs_time = []
-for i in range(0,len(canvas_parts)):
+'''
+stability_vs_time = th.stability(canvas_part, time_ranges, False,False)
+
+plt.figure()
+plt.plot(time_ranges[:-1]+time_interval/2, stability_vs_time)
+sns.despine()
+plt.ylabel('stability')
+plt.xlabel('Time [s]')
+plt.ylim([0, 1])
+plt.xlim([0, var.TIME_TOTAL])
+
+plt.savefig(os.path.join(os.getcwd(), 'figs', 'history_'+canvas_part.out_name(), 'stability.png'))
+'''
+
+file_path_stab = os.path.join(data_path, 'stability_all_canvas_compositions.pickle')
+
+'''
+for i in range(0, len(canvas_parts)):
     if i%100 == 0:
         print(i)
     mean_stability.append( th.stability(canvas_parts[i],np.asarray([0, var.TIME_WHITEONLY]),False,False)[0] )
 
-    stability_vs_time.append( th.stability(canvas_parts[i], time_ranges, False,False)[0] )
-    #print(mean_stability[i])
+    stability_vs_time.append( [th.stability(canvas_parts[i], time_ranges, False,False)] )
+    print(mean_stability[i])
 
-
-data_path = os.path.join(os.getcwd(),'data')
-file_path = os.path.join(data_path, 'stability_all_canvas_compositions.pickle')
-
-with open(file_path, 'wb') as handle:
+with open(file_path_stab, 'wb') as handle:
     pickle.dump([mean_stability,stability_vs_time],
                 handle,
                 protocol=pickle.HIGHEST_PROTOCOL)
-
 '''
-n, bins, patches = plt.hist(mean_stability, 75, range=[0,1], facecolor='blue', alpha=0.5, align='mid')
+
+
+with open(file_path_stab, 'rb') as handle:
+    [mean_stability,stability_vs_time] = pickle.load(handle)
+
+n, bins, patches = plt.hist(mean_stability, 80, range=[0,1], facecolor='blue', alpha=0.5, align='mid')
 
 sns.despine()
 plt.xlabel('mean stability')
 plt.ylabel('number of compositions')
 plt.xlim([0, 1])
 
-plt.savefig(os.path.join(os.getcwd(), 'figs','mean_stability_of_all_compositions.png'))
-'''
+plt.savefig(os.path.join(os.getcwd(), 'figs','mean_stability_for_all_compositions.png'))
 
 '''
 #time_ranges = np.append( np.arange(0, var.TIME_WHITEONLY, time_interval) , var.TIME_WHITEONLY)
@@ -96,26 +111,27 @@ stab_vs_time3 = th.stability(canvas_comp, time_ranges3, False, False)
 time_ranges4 = np.arange(0, var.TIME_WHITEONLY+time_interval/3-1e-4, time_interval/3)
 stab_vs_time4 = th.stability(canvas_comp, time_ranges4, False, False)
 
-plt.figure()
 plt.plot(time_ranges[:-1]+time_interval/2, stab_vs_time)# 1-(1-stab_vs_time)*100)
 plt.plot(time_ranges2[:-1]+time_interval*4/2, stab_vs_time2)#1-(1-stab_vs_time2)*25)
 plt.plot(time_ranges3[:-1]+time_interval*10/2, stab_vs_time3)#1-(1-stab_vs_time3)*10)
 plt.plot(time_ranges4[:-1]+time_interval/3/2, stab_vs_time4)#1-(1-stab_vs_time4)*300)
-sns.despine()
-plt.ylabel('stability')
-plt.xlabel('Time [s]')
-
-plt.savefig(os.path.join(os.getcwd(), 'figs', 'history_' + part_name, '_stability_vs_time_'+str(time_bins)+'timeRanges.png'))
 '''
 
 
-#cp.save_movie(os.path.join(os.getcwd(),'figs','history_'+part_name,'VsTime'), 10)
 
-#pr.disable()
-#s = io.StringIO()
-#sortby = SortKey.CUMULATIVE
-#ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-#ps.print_stats()
-#print(s.getvalue())
+
+
+
+plt.figure()
+for i in range(0,len(canvas_parts)):
+    plt.plot(time_ranges[:-1]+time_interval/2, stability_vs_time[i][0])
+sns.despine()
+plt.ylabel('stability')
+plt.xlabel('Time [s]')
+plt.ylim([0, 1])
+plt.xlim([0, var.TIME_TOTAL])
+
+plt.savefig(os.path.join(os.getcwd(), 'figs', 'stability_vs_time_all_compositions.png'))
+
 
 #cp.plot_compression(file_size_bmp, file_size_png, time_interval, total_time, part_name = part_name)
