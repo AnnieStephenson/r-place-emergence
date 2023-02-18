2678402ef2e1f57db77bfbbd1fa4ddc69b2454a7
AnnieStephenson <stephenson@g.harvard.edu>
2023-02-18 11:22:09 -0500

    modified ews to handle multiple transitions

--- rplacem/early_warning_signals.py
+++ rplacem/early_warning_signals.py
@@ -32,6 +32,7 @@ def calc_autocorrelation(x, time_window):
 def calc_ews(canvas_part_stat, time_window, state_vars = None):
     '''    
     calculates all the EWS vs time for all the state variables 
+    and all the transitions for a single canvas part
     
     time window is currently in units of time_interval, need to fix
     '''
@@ -41,14 +42,18 @@ def calc_ews(canvas_part_stat, time_window, state_vars = None):
         state_vars = [canvas_part_stat.entropy_vst,
                       canvas_part_stat.entropy_vst_bpmnorm,
                       canvas_part_stat.stability_vst,
-                      canvas_part_stat.num_pixchanges[0]]
-        
-    ews = np.zeros((3, len(state_vars),  canvas_part_stat.n_t_bins))
+                      canvas_part_stat.num_pixchanges]
+    
+    ews = np.zeros((3, canvas_part_stat.num_transitions, len(state_vars),  canvas_part_stat.n_t_bins))
     
     for i in range(len(state_vars)):
-        x = state_vars[i]
-        ews[0,i,:] = calc_variance(x, time_window)
-        ews[1,i,:] = calc_skewness(x, time_window)
-        ews[2,i,:] = calc_autocorrelation(x, time_window)
+        for j in range(canvas_part_stat.num_transitions):
+            if len(state_vars[i].shape)==2:
+                x = state_vars[i][j]
+            else: 
+                x = state_vars[i]
+            ews[0,j,i,:] = calc_variance(x, time_window)
+            ews[1,j,i,:] = calc_skewness(x, time_window)
+            ews[2,j,i,:] = calc_autocorrelation(x, time_window)
     
     return ews  
